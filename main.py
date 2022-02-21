f3246f91eb35cd291e230e5bb0fe64404abe1a1c
Udit <uditmishra.um@gmail.com>
2022-02-21 21:05:18 +0530

    Speed feature while the players hitting the paddle.

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -35,6 +35,7 @@ while game_is_on:
     # Detect Collision with the paddle and bounce back. Using distance method of turtle class.
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
+
 
     #Detect if the ball misses the paddle.
     if ball.xcor() > 380:
